f263ac648cdb4f9a8f15a202db79ed824ea65ea8
Ericky Vand <erickyvand@gmail.com>
2021-03-30 15:52:27 +0200

    feature(auth): add authentication

---
+++ src/redux/actions/userAction.js
@@ -0,0 +1,17 @@
+import { loginService, registerService } from '../../services/userService';
+import { LOGIN, REGISTER } from './actionType';
+
+export const registerAction = data => {
+	return {
+		type: REGISTER,
+		payload: registerService(data),
+	};
+};
+
+export const loginAction = data => {
+	console.log(data);
+	return {
+		type: LOGIN,
+		payload: loginService(data),
+	};
+};
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import loginReducer from './reducers/auth/loginReducer';
+import registerReducer from './reducers/auth/registerReducer';
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+	register: registerReducer,
+	login: loginReducer,
+});
 
 export default rootReducer;
